faa43badf67dc4aa61581972485880d6562e35f4
Isuru Vihan Harischandra <isuruvihan@gmail.com>
2022-05-31 11:48:36 +0530

    Borrow records modal bug fixed

--- controllers/borrowrecord.js
+++ controllers/borrowrecord.js
@@ -22,29 +22,38 @@ exports.createRecord = async (req, res) => {
     }
     if (qtyValidation) {
       if (datesValidation) {
-        BorrowRecord.create({borrowedOn: borrowDate, returnOn: returnDate, reason: reason, PlayerId: player})
-          .then((borrowRecord) => {
-            for (let i=1; i<quantities.length; i++) {
-              if (!(quantities[i]=='' || quantities[i]==0)) {
-                BorrowedItemGroup.create({quantity: quantities[i], BorrowRecordId: borrowRecord.id, ItemGroupId: i})
-                  .then(() => {
-                    ItemGroup.findAll({attributes: ['availableQuantity'], where: {id: i}})
-                      .then(async (item) => {
-                        const newQuantity = item[0].dataValues.availableQuantity - quantities[i];
-                        await ItemGroup.update({availableQuantity: newQuantity}, {where: {id: i}});
-                      });
+        BorrowRecord.findAll({where: {PlayerId: player}})
+          .then((record) => {
+            if (record.length > 0) {
+              return res.status(400).json({
+                message: "Please return previously borrowed items to create a new borrow record."
+              });
+            } else {
+              BorrowRecord.create({borrowedOn: borrowDate, returnOn: returnDate, reason: reason, PlayerId: player})
+                .then((borrowRecord) => {
+                  for (let i=1; i<quantities.length; i++) {
+                    if (!(quantities[i]=='' || quantities[i]==0)) {
+                      BorrowedItemGroup.create({quantity: quantities[i], BorrowRecordId: borrowRecord.id, ItemGroupId: i})
+                        .then(() => {
+                          ItemGroup.findAll({attributes: ['availableQuantity'], where: {id: i}})
+                            .then(async (item) => {
+                              const newQuantity = item[0].dataValues.availableQuantity - quantities[i];
+                              await ItemGroup.update({availableQuantity: newQuantity}, {where: {id: i}});
+                            });
+                        });
+                    }
+                  }
+                  return res.status(200).json({
+                    message: "Borrow record has been submitted successfully."
                   });
-               }
+                })
+                .catch((error) => {
+                  console.log("> CREATE BORROW RECORD ERROR: ", error);
+                  return res.status(400).json({
+                    message: "Unable to create the borrow record."
+                  });
+                });
             }
-            return res.status(200).json({
-              message: "Borrow record has been submitted successfully."
-            });
-          })
-          .catch((error) => {
-            console.log("> CREATE BORROW RECORD ERROR: ", error);
-            return res.status(400).json({
-              message: "Unable to create the borrow record."
-            });
           });
       } else {
         return res.status(400).json({
